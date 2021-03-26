650a59424a2d79d5cb66ec4b4f1b3ccfff876362
Vlad <dvladv_2013@mai.ru>
2021-03-26 18:03:15 +0300

    first

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const one = document.querySelector(".main_square_page_one")
+const t1 = new TimelineMax();
+
+
+function snow(){
+t1.fromTo(one,0,{width:"0px"},{width:"450px",ease: Power2.easeInOut})
+  .fromTo(
+    one,
+    0,
+    {height:"0px"},
+    {height:"450px",ease: Power2.easeInOut}
+  )
+}
+
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    leave(data) {
+      return gsap.to(data.current.container, {
+        //opacity: 0,
+        //delay: 0.2 
+      });
+    },
+    enter(data) {
+      
+      return gsap.from(data.next.container, {
+        //opacity: 0,
+        //delay: 0.2 
+      }),snow();
+    }
+  }]
+});
+
+
+      //   
+    //    const done = this.async();
+    //    setTimeout(function(){
+    //        done();
+    //    },700)
