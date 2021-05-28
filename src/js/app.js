38fb128e1f5e99126c8e7013af1ac21f411f87c4
Vlad <dvladv_2013@mail.ru>
2021-05-28 14:59:31 +0300

    new_page

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
-const one = document.querySelector(".main_square")
-const t1 = new TimelineMax();
-var elements = document.getElementsByClassName('header_square')[0];
-var small_1 = document.getElementsByClassName('main_square_small_1')[0];
-var small_2 = document.getElementsByClassName('main_square_small_2')[0];
-var small_3 = document.getElementsByClassName('main_square_small_3')[0];
-var small_4 = document.getElementsByClassName('main_square_small_4')[0];
-var small_5 = document.getElementsByClassName('main_square_small_5')[0];
-var small_6 = document.getElementsByClassName('main_square_small_6')[0];
-var small_7 = document.getElementsByClassName('main_square_small_7')[0];
-var small_8 = document.getElementsByClassName('main_square_small_8')[0];
-var small_9 = document.getElementsByClassName('main_square_small_9')[0];
+"use strict";
+const one = document.querySelector(".main_square");
+//const t1 = new TimelineMax();
+let elements = document.getElementsByClassName('header_square')[0];
+let small_1 = document.getElementsByClassName('main_square_small_1')[0];
+let small_2 = document.getElementsByClassName('main_square_small_2')[0];
+let small_3 = document.getElementsByClassName('main_square_small_3')[0];
+let small_4 = document.getElementsByClassName('main_square_small_4')[0];
+let small_5 = document.getElementsByClassName('main_square_small_5')[0];
+let small_6 = document.getElementsByClassName('main_square_small_6')[0];
+let small_7 = document.getElementsByClassName('main_square_small_7')[0];
+let small_8 = document.getElementsByClassName('main_square_small_8')[0];
+let small_9 = document.getElementsByClassName('main_square_small_9')[0];
 
 
 
@@ -18,16 +19,6 @@ var small_9 = document.getElementsByClassName('main_square_small_9')[0];
 // for (let i = 0; i < 1; i++) {
 //   elements.addEventListener('click', change_color);
 // }
-
-
-
-
-
-
-
-
-
-
 
 
 // function snow(){
@@ -62,182 +53,182 @@ var small_9 = document.getElementsByClassName('main_square_small_9')[0];
 
 elements.onclick = function() {
   function small7_7() {
-  small_7.style.transition = 'all linear 0.5s'
-  small_7.style.borderImage = 'linear-gradient(to left top, rgb(48,182,249), 70%,rgb(22,95,252))'
-  small_7.style.backgroundColor = 'black'
-  small_7.style.borderImageSlice = '1'
+  small_7.style.transition = 'all linear 0.5s';
+  small_7.style.borderImage = 'linear-gradient(to left top, rgb(48,182,249), 70%,rgb(22,95,252))';
+  small_7.style.backgroundColor = 'black';
+  small_7.style.borderImageSlice = '1';
   }
-  setTimeout(small7_7,100)
+  setTimeout(small7_7,100);
   function small_7_1(){
-  small_7.style.transition = 'all linear 0.5s'
-  small_7.style.borderImage = 'none'
-  small_7.style.backgroundColor = 'white'
+  small_7.style.transition = 'all linear 0.5s';
+  small_7.style.borderImage = 'none';
+  small_7.style.backgroundColor = 'white';
   }
-  setTimeout(small_7_1,1300)
+  setTimeout(small_7_1,1300);
   
   function small_7_2(){    
     small_7.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_7_2,1310)
+  setTimeout(small_7_2,1310);
 
   function small4_4() {
-  small_4.style.transition = 'all linear 0.5s'
-  small_4.style.borderImage = 'linear-gradient(to left top, rgb(22,95,252), 70%, rgb(101,46,148))'
-  small_4.style.backgroundColor = 'black'
-  small_4.style.borderImageSlice = '1'
+  small_4.style.transition = 'all linear 0.5s';
+  small_4.style.borderImage = 'linear-gradient(to left top, rgb(22,95,252), 70%, rgb(101,46,148))';
+  small_4.style.backgroundColor = 'black';
+  small_4.style.borderImageSlice = '1';
   }
-  setTimeout(small4_4,200)
+  setTimeout(small4_4,200);
 
   function small_4_1(){
-  small_4.style.transition = 'all linear 0.5s'
-  small_4.style.borderImage = 'none'
-  small_4.style.backgroundColor = 'white'
+  small_4.style.transition = 'all linear 0.5s';
+  small_4.style.borderImage = 'none';
+  small_4.style.backgroundColor = 'white';
   }
-  setTimeout(small_4_1,1400)
+  setTimeout(small_4_1,1400);
   
   function small_4_2(){    
     small_4.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_4_2,1410)
+  setTimeout(small_4_2,1410);
 
 
   function small8_8() {
-  small_8.style.transition = 'all linear 0.5s'
-  small_8.style.borderImage = 'linear-gradient(to right top, rgb(48,182,249), 30%,rgb(57,181,74))'
-  small_8.style.backgroundColor = 'black'
-  small_8.style.borderImageSlice = '1'
+  small_8.style.transition = 'all linear 0.5s';
+  small_8.style.borderImage = 'linear-gradient(to right top, rgb(48,182,249), 30%,rgb(57,181,74))';
+  small_8.style.backgroundColor = 'black';
+  small_8.style.borderImageSlice = '1';
   }
-  setTimeout(small8_8,200)
+  setTimeout(small8_8,200);
 
   function small_8_1(){
-  small_8.style.transition = 'all linear 0.5s'
-  small_8.style.borderImage = 'none'
-  small_8.style.backgroundColor = 'white'
+  small_8.style.transition = 'all linear 0.5s';
+  small_8.style.borderImage = 'none';
+  small_8.style.backgroundColor = 'white';
   }
-  setTimeout(small_8_1,1400)
+  setTimeout(small_8_1,1400);
   
   function small_8_2(){    
     small_8.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_8_2,1410)
+  setTimeout(small_8_2,1410);
 
   function small1_1() {
-  small_1.style.transition = 'all linear 0.5s'
-  small_1.style.borderImage = 'linear-gradient(to right top, rgb(102,45,148), 70%,rgb(232,2,140))'
-  small_1.style.backgroundColor = 'black'
-  small_1.style.borderImageSlice = '1'
+  small_1.style.transition = 'all linear 0.5s';
+  small_1.style.borderImage = 'linear-gradient(to right top, rgb(102,45,148), 70%,rgb(232,2,140))';
+  small_1.style.backgroundColor = 'black';
+  small_1.style.borderImageSlice = '1';
   }
-  setTimeout(small1_1,300)
+  setTimeout(small1_1,300);
 
   function small_1_1(){
-  small_1.style.transition = 'all linear 0.5s'
-  small_1.style.borderImage = 'none'
-  small_1.style.backgroundColor = 'white'
+  small_1.style.transition = 'all linear 0.5s';
+  small_1.style.borderImage = 'none';
+  small_1.style.backgroundColor = 'white';
   }
-  setTimeout(small_1_1,1500)
+  setTimeout(small_1_1,1500);
   
   function small_1_2(){    
     small_1.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_1_2,1510)
+  setTimeout(small_1_2,1510);
 
   function small5_5() {
-  small_5.style.transition = 'all linear 0.5s'
-  small_5.style.borderImage = 'linear-gradient(to right top, rgb(254,199,7), 70%,rgb(238,50,74))'
-  small_5.style.backgroundColor = 'black'
-  small_5.style.borderImageSlice = '1'
+  small_5.style.transition = 'all linear 0.5s';
+  small_5.style.borderImage = 'linear-gradient(to right top, rgb(254,199,7), 70%,rgb(238,50,74))';
+  small_5.style.backgroundColor = 'black';
+  small_5.style.borderImageSlice = '1';
   }
-  setTimeout(small5_5,300)
+  setTimeout(small5_5,300);
 
   function small_5_1(){
-  small_5.style.transition = 'all linear 0.5s'
-  small_5.style.borderImage = 'none'
-  small_5.style.backgroundColor = 'white'
+  small_5.style.transition = 'all linear 0.5s';
+  small_5.style.borderImage = 'none';
+  small_5.style.backgroundColor = 'white';
   }
-  setTimeout(small_5_1,1500)
+  setTimeout(small_5_1,1500);
   
   function small_5_2(){    
     small_5.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_5_2,1510)
+  setTimeout(small_5_2,1510);
 
   function small9_9() {
-  small_9.style.transition = 'all linear 0.5s'
-  small_9.style.borderImage = 'linear-gradient(to top right, rgb(58,182,73), 70%,rgb(250,204,6))'
-  small_9.style.backgroundColor = 'black'
-  small_9.style.borderImageSlice = '1'
+  small_9.style.transition = 'all linear 0.5s';
+  small_9.style.borderImage = 'linear-gradient(to top right, rgb(58,182,73), 70%,rgb(250,204,6))';
+  small_9.style.backgroundColor = 'black';
+  small_9.style.borderImageSlice = '1';
   }
-  setTimeout(small9_9,300)
+  setTimeout(small9_9,300);
 
   function small_9_1(){
-  small_9.style.transition = 'all linear 0.5s'
-  small_9.style.borderImage = 'none'
-  small_9.style.backgroundColor = 'white'
+  small_9.style.transition = 'all linear 0.5s';
+  small_9.style.borderImage = 'none';
+  small_9.style.backgroundColor = 'white';
   }
-  setTimeout(small_9_1,1500)
+  setTimeout(small_9_1,1500);
   
   function small_9_2(){    
     small_9.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_9_2,1510)
+  setTimeout(small_9_2,1510);
 
   function small6_6() {
-  small_6.style.transition = 'all linear 0.5s'
-  small_6.style.borderImage = 'linear-gradient(to top right, rgb(247,148,30), 30%,rgb(239,226,0))'
-  small_6.style.backgroundColor = 'black'
-  small_6.style.borderImageSlice = '1'
+  small_6.style.transition = 'all linear 0.5s';
+  small_6.style.borderImage = 'linear-gradient(to top right, rgb(247,148,30), 30%,rgb(239,226,0))';
+  small_6.style.backgroundColor = 'black';
+  small_6.style.borderImageSlice = '1';
   }
-  setTimeout(small6_6,400)
+  setTimeout(small6_6,400);
 
   function small_6_1(){
-  small_6.style.transition = 'all linear 0.5s'
-  small_6.style.borderImage = 'none'
-  small_6.style.backgroundColor = 'white'
+  small_6.style.transition = 'all linear 0.5s';
+  small_6.style.borderImage = 'none';
+  small_6.style.backgroundColor = 'white';
   }
-  setTimeout(small_6_1,1600)
+  setTimeout(small_6_1,1600);
   
   function small_6_2(){    
     small_6.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_6_2,1610)
+  setTimeout(small_6_2,1610);
 
   function small2_2() {
-  small_2.style.transition = 'all linear 0.5s'
-  small_2.style.borderImage = 'linear-gradient(to right top, rgb(109,43,145), 30%,rgb(236,0,140))'
-  small_2.style.backgroundColor = 'black'
-  small_2.style.borderImageSlice = '1'
+  small_2.style.transition = 'all linear 0.5s';
+  small_2.style.borderImage = 'linear-gradient(to right top, rgb(109,43,145), 30%,rgb(236,0,140))';
+  small_2.style.backgroundColor = 'black';
+  small_2.style.borderImageSlice = '1';
   }
-  setTimeout(small2_2,400)
+  setTimeout(small2_2,400);
 
   function small_2_1(){
-  small_2.style.transition = 'all linear 0.5s'
-  small_2.style.borderImage = 'none'
-  small_2.style.backgroundColor = 'white'
+  small_2.style.transition = 'all linear 0.5s';
+  small_2.style.borderImage = 'none';
+  small_2.style.backgroundColor = 'white';
   }
-  setTimeout(small_2_1,1600)
+  setTimeout(small_2_1,1600);
   
   function small_2_2(){    
     small_2.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_2_2,1610)
+  setTimeout(small_2_2,1610);
 
   function small3_3() {
-  small_3.style.transition = 'all linear 0.5s'
-  small_3.style.borderImage = 'linear-gradient(to bottom right, rgb(236,3,136), 30%,rgb(239,65,54))'
-  small_3.style.backgroundColor = 'black'
-  small_3.style.borderImageSlice = '1'
+  small_3.style.transition = 'all linear 0.5s';
+  small_3.style.borderImage = 'linear-gradient(to bottom right, rgb(236,3,136), 30%,rgb(239,65,54))';
+  small_3.style.backgroundColor = 'black';
+  small_3.style.borderImageSlice = '1';
   }
-  setTimeout(small3_3,500)
+  setTimeout(small3_3,500);
 
   function small_3_1(){
-  small_3.style.transition = 'all linear 0.5s'
-  small_3.style.borderImage = 'none'
-  small_3.style.backgroundColor = 'white'
+  small_3.style.transition = 'all linear 0.5s';
+  small_3.style.borderImage = 'none';
+  small_3.style.backgroundColor = 'white';
   }
-  setTimeout(small_3_1,1700)
+  setTimeout(small_3_1,1700);
   
   function small_3_2(){    
     small_3.setAttribute('style', 'background-color:!important');
   }
-  setTimeout(small_3_2,1710)
+  setTimeout(small_3_2,1710);
 };
