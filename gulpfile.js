3903a243ad8965109982f6d399d2362d9a725747
Vlad <dvladv_2013@mail.ru>
2021-06-03 14:18:54 +0300

    new_svg

--- gulpfile.js
+++ gulpfile.js
@@ -23,12 +23,13 @@ gulp.task('styles', function() {
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
-})
+});
 
 gulp.task('watch', function(){
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src/index.html").on("change",browserSync.reload)
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/index.html").on("change",browserSync.reload);
     gulp.watch("src/html/*.html").on("change",browserSync.reload);
+    gulp.watch("src/html/art/*.html").on("change",browserSync.reload);
 });
 
 gulp.task('default', gulp.parallel('watch','server','styles'));
