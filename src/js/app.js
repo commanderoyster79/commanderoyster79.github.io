b0edbfb541ae9c1b3c3c68bf4e87f757fd8a606d
Vlad <dvladv_2013@mail.ru>
2021-06-08 17:44:21 +0300

    new_scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
     const leftLine = document.querySelector('.yaga_box_left');
     const rightLine = document.querySelector('.yaga_box_right');
+    const yaga = document.querySelectorAll('.yaga_box');
 
-
+    
     function moveLine (atribute,valueX,valueY,valueZ) {
         atribute.style.transform = `translate3d(${valueX}px,${valueY}px,${valueZ}px)`;
     }
 
+    yaga.forEach(item => {
+        console.log(item[1]);
+    });
 
     window.addEventListener('scroll', function() {
         let scroll = window.pageYOffset || document.documentElement.scrollTop ||
                  document.body.scrollTop || 0;
+        
         if (scroll < 1100) {
             moveLine(leftLine,(scroll-1100),0,0);
             moveLine(rightLine,(-scroll+1100),0,0);
@@ -20,10 +25,16 @@ window.addEventListener('DOMContentLoaded', () => {
             moveLine(rightLine,0,0,0);
         }
         
+        if (scroll >= 300 && scroll <= 1000 ) {
+            
+        }
+
+        
         
         console.log(scroll);
+        
 
-        //400-1100
+        //300-1100
 
         //-362  0
 
