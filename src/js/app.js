a532711f331ac2e4864c2a7194d746dd9d7fdc8c
Vlad <dvladv_2013@mail.ru>
2021-06-08 18:06:39 +0300

    new_scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
     const leftLine = document.querySelector('.yaga_box_left');
     const rightLine = document.querySelector('.yaga_box_right');
-    const yaga = document.querySelectorAll('.yaga_box');
+    const yagaImg = document.querySelector('.yaga_img_yaga');
+    const yagaCache = document.querySelector('.yaga_cache_move');
 
     
-    function moveLine (atribute,valueX,valueY,valueZ) {
+    function move (atribute,valueX,valueY,valueZ) {
         atribute.style.transform = `translate3d(${valueX}px,${valueY}px,${valueZ}px)`;
     }
 
-    yaga.forEach(item => {
-        console.log(item[1]);
-    });
 
     window.addEventListener('scroll', function() {
         let scroll = window.pageYOffset || document.documentElement.scrollTop ||
                  document.body.scrollTop || 0;
         
         if (scroll < 1100) {
-            moveLine(leftLine,(scroll-1100),0,0);
-            moveLine(rightLine,(-scroll+1100),0,0);
+            move(leftLine,(scroll-1100),0,0);
+            move(rightLine,(-scroll+1100),0,0);
         }
         if (scroll == 1100) {
-            moveLine(leftLine,0,0,0);
-            moveLine(rightLine,0,0,0);
+            move(leftLine,0,0,0);
+            move(rightLine,0,0,0);
         }
         
-        if (scroll >= 300 && scroll <= 1000 ) {
-            
+        if (scroll >= 300 && scroll <= 1300 ) {
+            move(yagaImg,(-1300+scroll),0,0);
+        }
+
+        if (scroll >= 1700 && scroll <= 2900) {
+            move(yagaCache,(+380-scroll/5),0,0);
         }
 
         
@@ -38,7 +40,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
         //-362  0
 
-        //0 -670
+        //1700 - 2900
+
+
 
 
       });
