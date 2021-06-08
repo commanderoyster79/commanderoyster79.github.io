5ea432c9666de20367450f012d0949c9a35547df
Vlad <dvladv_2013@mail.ru>
2021-06-08 17:13:08 +0300

    new_scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
     const leftLine = document.querySelector('.yaga_box_left');
+    const rightLine = document.querySelector('.yaga_box_right');
+
+
+    function moveLine (atribute,valueX,valueY,valueZ) {
+        atribute.style.transform = `translate3d(${valueX}px,${valueY}px,${valueZ}px)`;
+    }
 
 
     window.addEventListener('scroll', function() {
         let scroll = window.pageYOffset || document.documentElement.scrollTop ||
                  document.body.scrollTop || 0;
-        leftLine.style.transform = 'translate3d('+ (scroll-1100) +'px,0,0)';
+        if (scroll < 1100) {
+            moveLine(leftLine,(scroll-1100),0,0);
+            moveLine(rightLine,(-scroll+1100),0,0);
+        }
+        if (scroll == 1100) {
+            moveLine(leftLine,0,0,0);
+            moveLine(rightLine,0,0,0);
+        }
         
         
         console.log(scroll);
