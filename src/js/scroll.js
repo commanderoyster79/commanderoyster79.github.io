196660f8700ad40f6df7420cbfbb9d3566a78f40
Vlad <dvladv_2013@mail.ru>
2021-06-23 18:25:23 +0300

    new_icons_on_logofolio

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,6 +3,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const moscow = document.querySelector('.moscow');
     const startool = document.querySelector('.startool');
     const litres = document.querySelector('.litres');
+    const breesal = document.querySelector('.breesal');
+    const trueMagazine = document.querySelector('.trueMagazine');
+    const ligetdukat = document.querySelector('.ligetdukat');
+    const pskov = document.querySelector('.pskov');
+    const whiteblack = document.querySelector('.whiteblack');
+    const weaving = document.querySelector('.weaving');
+    const victory = document.querySelector('.victory');
+    const kargopol = document.querySelector('.kargopol');
+    const dunskaya = document.querySelector('.dunskaya');
+    const almatel = document.querySelector('.almatel');
+    
 
     function move (atribute,valueX,valueY,valueZ) {
         atribute.style.transform = `translate3d(${valueX}px,${valueY}px,${valueZ}px)`;
@@ -17,6 +28,16 @@ window.addEventListener('DOMContentLoaded', () => {
     move (moscow,0,scroll,0);
     move (startool,0,scroll,0);
     move (litres,0,scroll,0);
+    move (breesal,0,scroll,0);
+    move (trueMagazine,0,scroll,0);
+    move (ligetdukat,0,scroll,0);
+    move (pskov,0,scroll,0);
+    move (whiteblack,0,scroll,0);
+    move (weaving,0,scroll,0);
+    move (victory,0,scroll,0);
+    move (kargopol,0,scroll,0);
+    move (dunskaya,0,scroll,0);
+    move (almatel,0,scroll,0);
 
     if (scroll >= 500) {
         move (moscow,0,-scroll+1000,0);
@@ -28,6 +49,46 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (scroll >= 1500) {
         move (litres,0,-scroll+3000,0);
+    }
+
+    if (scroll >= 2000) {
+        move (breesal,0,-scroll+4000,0);
+    }
+
+    if (scroll >= 2500) {
+        move (trueMagazine,0,-scroll+5000,0);
+    }
+
+    if (scroll >= 3000) {
+        move (ligetdukat,0,-scroll+6000,0);
+    }
+
+    if (scroll >= 3500) {
+        move (pskov,0,-scroll+7000,0);
+    }
+
+    if (scroll >= 4000) {
+        move (whiteblack,0,-scroll+8000,0);
+    }
+
+    if (scroll >= 4500) {
+        move (weaving,0,-scroll+9000,0);
+    }
+
+    if (scroll >= 5000) {
+        move (victory,0,-scroll+10000,0);
+    }
+
+    if (scroll >= 5500) {
+        move (kargopol,0,-scroll+11000,0);
+    }
+
+    if (scroll >= 6000) {
+        move (dunskaya,0,-scroll+12000,0);
+    }
+
+    if (scroll >= 6500) {
+        move (almatel,0,-scroll+13000,0);
     }
     
 
