dc95d3b7844d61e9cb7b57ee39bddbd18b124079
Vlad <dvladv_2013@mail.ru>
2021-07-05 13:52:41 +0300

    animation

---
+++ src/js/swap_img.js
@@ -0,0 +1,53 @@
+const imgLinksLitres = [
+    'url(../../icons/logo/04_LITRES_01.webp)',
+    'url(../../icons/logo/05_LITRES_02.webp)',
+    'url(../../icons/logo/06_LITRES_03.webp)',
+    'url(../../icons/logo/07_LITRES_04.webp)'
+];
+
+
+
+const whiteblack = document.querySelector('.whiteblack');
+const whiteblackImg = document.querySelector('.whiteblack_box_img');
+const whiteblackDescr = document.querySelector('.whiteblack_descr');
+const whiteblackDescrLine = document.querySelector('.whiteblack_descr_box_line');
+const whiteblackDescrNumeretion = document.querySelector('.whiteblack_descr_box_numeration');
+
+
+const victoryImg = document.querySelector('.victory_box_img');
+
+
+
+const delay = 2000;
+let currentIndex = 0;
+let indexWB = 0;
+
+    setInterval(function() {
+        document.querySelector('.litres_box_img').style.backgroundImage = imgLinksLitres[currentIndex];
+        currentIndex++;
+        if(currentIndex >= imgLinksLitres.length) {
+            currentIndex = 0;
+        }
+        if (indexWB == 0) {
+            whiteblack.style.backgroundColor = 'white';
+            whiteblackDescr.style.color = 'black';
+            whiteblackDescrLine.style.backgroundColor = 'black';
+            whiteblackDescrNumeretion.style.color = 'black';
+            whiteblackImg.style.backgroundImage = 'url(../../icons/logo/17_CHERNOE.webp)';
+            victoryImg.style.backgroundImage = 'url(../../icons/logo/19_75_let_01.webp)';
+            indexWB++;
+        } else {
+            whiteblack.style.backgroundColor = 'black';
+            whiteblackDescr.style.color = 'white';
+            whiteblackDescrLine.style.backgroundColor = 'white';
+            whiteblackDescrNumeretion.style.color = 'white';
+            whiteblackImg.style.backgroundImage = 'url(../../icons/logo/16_BELOE.webp)';
+            victoryImg.style.backgroundImage = 'url(../../icons/logo/20_75_let_02.webp)';
+            indexWB--;
+        }
+        
+    }, delay);
+
+
+
+    
