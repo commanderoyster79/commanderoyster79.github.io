34e389119fb08d62d1b99e7ef1833e8fd78ce8e9
Vlad <dvladv_2013@mail.ru>
2021-07-13 23:03:12 +0300

    new_soap_page

--- src/js/soap_scroll.js
+++ src/js/soap_scroll.js
@@ -3,12 +3,89 @@ window.addEventListener('DOMContentLoaded', () => {
     const rightMainsoapLine = document.querySelector('.soap_box_right');
     const headerMainsoapBlack = document.querySelector('.soap_box_header_black');
     const descr = document.querySelector('.descr');
-    
+    const logosoapDescr = document.querySelector('.logosoap_descr');
+    const moodblockDescr = document.querySelector('.moodblock_descr');
+    const craftPaperBlock = document.querySelectorAll('.craft_paper_block');
+    const craftPaperBlockEight = document.querySelector('.craft_paper_block_8');
+
+
+    console.log(craftPaperBlock);
+
+
+
+        
+        
 
     
 
+
+    function swapCraftBlock() {
+        setTimeout(function run() {
+            setTimeout(() => {
+                for (let i = 0; i <= craftPaperBlock.length - 1; i++) {
+                    setTimeout(function () {
+                        if (i == 5) {
+                            allClose(craftPaperBlock[9]);
+                        }
+                        else if (i == 6) {
+                            allClose(craftPaperBlock[8]);
+                        }
+                        else if (i == 7) {
+                            allClose(craftPaperBlock[7]);
+                        }
+                        else if (i == 8) {
+                            allClose(craftPaperBlock[6]);
+                            craftPaperBlockEight.style.backgroundImage = 'url(../../img/soap/06_Raport_white.png)';
+                        }
+                        else if (i == 9) {
+                            allClose(craftPaperBlock[5]);
+                        } else {
+                            allClose(craftPaperBlock[i]);
+                        } 
+                    }, 100 * i);
+                } 
+            }, 5000);
+            setTimeout(() => {
+                craftPaperBlockEight.style.backgroundImage = 'url(../../img/soap/07_Raport_brown.png)';
+            }, 3000);
+            for (let i = 0; i <= craftPaperBlock.length - 1; i++) {
+                setTimeout(function () {
+                    if (i == 5) {
+                        allOpen(craftPaperBlock[9]);
+                    }
+                    else if (i == 6) {
+                        allOpen(craftPaperBlock[8]);
+                    }
+                    else if (i == 7) {
+                        allOpen(craftPaperBlock[7]);
+                    }
+                    else if (i == 8) {
+                        allOpen(craftPaperBlock[6]);
+                    }
+                    else if (i == 9) {
+                        allOpen(craftPaperBlock[5]);
+                    } else {
+                        allOpen(craftPaperBlock[i]);
+                    } 
+                }, 100 * i);
+            }
+            setTimeout(run, 7000);
+        },7000);
+    }
+
     
 
+
+            
+
+    
+    function allClose(atribute) {
+        atribute.style.opacity = '0';
+    }
+
+    function allOpen(atribute) {
+        atribute.style.opacity = '1';
+    }
 
     function move (atribute,valueX,valueY,valueZ) {
         atribute.style.transform = `translate3d(${valueX}px,${valueY}px,${valueZ}px)`;
@@ -23,13 +100,14 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    hiddenHeader(headerMainsoapBlack,0);
-    transitionHeader(headerMainsoapBlack, 1.5);
-    move(leftMainsoapLine,0,0,0);
-    transitionHeader(leftMainsoapLine, 0.7);
-    move(rightMainsoapLine,0,0,0);
-    transitionHeader(rightMainsoapLine, 0.7);
-    descr.style.opacity = '0';
+    
+
+
+
+
+
+
+
 
 
 
@@ -47,6 +125,37 @@ window.addEventListener('DOMContentLoaded', () => {
             descr.classList.add('slideUp');
             descr.style.opacity = '1';
         }
+
+        if (scroll <= 1300) {
+            logosoapDescr.style.opacity = '0';
+            logosoapDescr.classList.remove('slideUp');
+            transitionHeader(descr, 0.5);
+        } else {
+            logosoapDescr.classList.add('slideUp');
+            logosoapDescr.style.opacity = '1';
+        }
+
+        if (scroll <= 2400) {
+            moodblockDescr.style.opacity = '0';
+            moodblockDescr.classList.remove('slideUp');
+            transitionHeader(descr, 0.5);
+        } else {
+            moodblockDescr.classList.add('slideUp');
+            moodblockDescr.style.opacity = '1';
+        }
         console.log(scroll);
-    });  
+        
+    }); 
+    
+    
+    swapCraftBlock();
+    hiddenHeader(headerMainsoapBlack,0);
+    transitionHeader(headerMainsoapBlack, 1.5);
+    move(leftMainsoapLine,0,0,0);
+    transitionHeader(leftMainsoapLine, 0.7);
+    move(rightMainsoapLine,0,0,0);
+    transitionHeader(rightMainsoapLine, 0.7);
+    descr.style.opacity = '0';
+    logosoapDescr.style.opacity = '0';
+    moodblockDescr.style.opacity = '0';
 }); 
